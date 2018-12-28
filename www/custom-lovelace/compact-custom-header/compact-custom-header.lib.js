86b97367d165265d999da6c2de71fc57d68dc872
DavidFW1960 <36390762+DavidFW1960@users.noreply.github.com>
2018-12-28 13:22:03 +1100

    update automations

--- www/custom-lovelace/compact-custom-header/compact-custom-header.lib.js
+++ www/custom-lovelace/compact-custom-header/compact-custom-header.lib.js
@@ -1,15 +1,12 @@
 // Avoid "already defined" errors when navigating away from Lovelace and back.
 if (typeof doc_root == 'undefined') {
-  var app_layout, card_options, cch_card,clock, clock_format,
-      clock_width, column, column_container, container, container_div,
-      div_view, doc_root, drawer_layout, edit_mode, hui_root, hui_view, icon,
-      iron_icon, love_lace, main, menu_btn_sr, menu_button, menu_icon,
-      menu_icon_sr, menu_iron_icon, notify_btn_sr, notify_button, notify_icon,
-      notify_icon_sr, notify_indicator, notify_iron_icon, options_button,
-      options_icon, options_icon_sr, options_iron_icon, pad, pages, panel,
-      proceed, shadow_root, shift_amount, tabs, tabs_container, tabs_sr,
-      tab_chevron, tab_count, toolbar, voice_btn_sr, voice_button, voice_icon,
-      voice_icon_sr, voice_iron_icon;
+  var app_layout, card, clock, clock_format,clock_width, div_view, doc_root,
+      drawer_layout, edit_mode, hui_root, icon, iron_icon, love_lace, main,
+      menu_btn, menu_icon, menu_iron_icon, notify_btn, notify_icon,
+      notify_indicator, notify_iron_icon, options_button, options_icon,
+      options_iron_icon, pad, pages, panel, proceed, shift_amount, tabs,
+      tabs_container, chevron, tabs_count, toolbar, voice_btn, voice_icon,
+      voice_iron_icon;
 }
 // Try so that if we're not on a lovelace page it won't continue to run...
 // since some elements only exist on lovelace & we insert script in doc head.
@@ -30,122 +27,69 @@ try {
   console.log(e);
 }
 if (proceed) {
-  // Find elements to style.
-  menu_button = hui_root.querySelector('ha-menu-button');
-  menu_btn_sr = menu_button.shadowRoot;
-  menu_icon = menu_btn_sr.querySelector('paper-icon-button');
-  menu_icon_sr = menu_icon.shadowRoot;
-  menu_iron_icon = menu_icon_sr.querySelector('iron-icon');
-  voice_button = hui_root.querySelector('ha-start-voice-button');
-  voice_btn_sr = voice_button.shadowRoot;
-  voice_icon = voice_btn_sr.querySelector('paper-icon-button');
-  voice_icon_sr = voice_icon.shadowRoot;
-  voice_iron_icon = voice_icon_sr.querySelector('iron-icon');
-  notify_button = hui_root.querySelector('hui-notifications-button');
-  notify_btn_sr = notify_button.shadowRoot;
-  notify_icon = notify_btn_sr.querySelector('paper-icon-button');
-  notify_icon_sr = notify_icon.shadowRoot;
-  notify_iron_icon = notify_icon_sr.querySelector('iron-icon');
-  notify_indicator = notify_btn_sr.querySelector('[class="indicator"]');
+  // Get elements to style.
+  menu_btn = hui_root.querySelector('ha-menu-button');
+  menu_icon = menu_btn.shadowRoot.querySelector('paper-icon-button');
+  menu_iron_icon = menu_icon.shadowRoot.querySelector('iron-icon');
+  notify_btn = hui_root.querySelector('hui-notifications-button');
+  notify_icon = notify_btn.shadowRoot.querySelector('paper-icon-button');
+  notify_iron_icon = notify_icon.shadowRoot.querySelector('iron-icon');
+  notify_indicator = notify_btn.shadowRoot.querySelector('[class="indicator"]');
+  voice_btn = hui_root.querySelector('ha-start-voice-button');
+  voice_icon = voice_btn.shadowRoot.querySelector('paper-icon-button');
+  voice_iron_icon = voice_icon.shadowRoot.querySelector('iron-icon');
   options_button = hui_root.querySelector('paper-menu-button');
   options_icon = options_button.querySelector('paper-icon-button');
-  options_icon_sr = options_icon.shadowRoot;
-  options_iron_icon = options_icon_sr.querySelector('iron-icon');
-  toolbar = hui_root.querySelectorAll('app-toolbar');
+  options_iron_icon = options_icon.shadowRoot.querySelector('iron-icon');
   tabs = hui_root.querySelector('paper-tabs');
-  tabs_sr = hui_root.querySelector('paper-tabs').shadowRoot;
-  tab_count = tabs.querySelectorAll('paper-tab');
-  tabs_container = tabs_sr.getElementById('tabsContainer');
-  tab_chevron = tabs_sr.querySelectorAll('[icon^="paper-tabs:chevron"]');
-  let card_in_panel = false;
-  let card_found = false;
-  let container_edit = false;
-  let alone = false;
-
-  // If card isn't contained in panel view or a container type card...
-  // get the column elements.
-  if (div_view.querySelector('hui-view') != null) {
-    hui_view = div_view.querySelector('hui-view').shadowRoot;
-    column_container = hui_view.querySelector('[id="columns"]');
-    column = column_container.querySelectorAll('[class="column"]');
-  } else {
-    card_in_panel = true;
-  }
-
-  // Find the compact-custom-header card under normal circumstances.
-  if (div_view.querySelector('hui-view') != null && !card_in_panel) {
-    for (let i = 0; i < column.length; i++) {
-      if (column[i].querySelector('compact-custom-header')) {
-        cch_card = column[i].querySelector('compact-custom-header');
-        cch_card.style.cssText = 'display:none';
-        // Is the card the only one in the column?
-        if (column[i].children.length == 1) {
-          cch_card.parentNode.style.cssText = alone ? 'display:none' : '';
-          alone = true;
-        }
-        card_found = true;
-        break;
-      }
-    }
-  }
-
-  // Card placement changes depending on location and if in edit view.
-  // If inside a container card and in edit view:
-  if (!card_in_panel && !card_found) {
-    container_edit = (column[0].querySelector('hui-card-options') != null);
-  }
-  if (!card_in_panel && !card_found && container_edit) {
-    card_options = column[0].querySelector('hui-card-options');
-    container = card_options.querySelector('*').shadowRoot;
-    container_div = container.querySelector('div');
-    cch_card = container_div.querySelector('compact-custom-header');
-    alone = false;
-  // If inside container card not in edit view
-  } else if (!card_found && !card_in_panel) {
-    container = column[0].querySelector('*').shadowRoot;
-    container_div = container.querySelector('div');
-    cch_card = container_div.querySelector('compact-custom-header');
-    alone = false;
-  // If inside panel view.
-  } else if (card_in_panel){
-    container = div_view.querySelector('*').shadowRoot;
-    container_div = container.querySelector('div');
-    cch_card = container_div.querySelector('compact-custom-header');
-    alone = false;
-  }
+  tabs_count = tabs.querySelectorAll('paper-tab');
+  tabs_container = tabs.shadowRoot.getElementById('tabsContainer');
+  chevron = tabs.shadowRoot.querySelectorAll('[icon^="paper-tabs:chevron"]');
+  toolbar = hui_root.querySelectorAll('app-toolbar');
 
   // If multiple toolbars exist & 2nd one is displayed, edit mode is active.
-  // If there is only one toolbar, toolbar[1] doesn't exist yet and will error.
   if (toolbar.length > 1) {
     edit_mode = toolbar[1].style.cssText != 'display: none;' ? true : false;
   } else {
     edit_mode = false;
   }
 
-  if (edit_mode && cch_card != null) {
-    // Show card in edit mode (remove 'display:none').
-    cch_card.style.cssText = '';
-    cch_card.innerHTML = `
-      <svg style="float:left;height:30px;padding:15px;padding-right:5px;"
-      viewBox="0 0 24 24"><path fill="var(--primary-text-color)"
-      d="M12,7L17,12H14V16H10V12H7L12,7M19,21H5A2,2 0 0,1 3,19V5A2,2 0 0,1 5,
-      3H19A2,2 0 0,1 21,5V19A2,2 0 0,1 19,21M19,19V5H5V19H19Z"></path></svg>
-      <h2 style="margin:auto;padding:20px;">Compact Custom Header</h2>
-    `;
-    cch_card.parentNode.style.cssText = 'background-color:var(--primary-color)';
-  } else if (cch_card != null) {
-    // Hide card outside of edit mode.
-    cch_card.style.cssText = 'display:none';
-    cch_card.innerHTML = '';
-    // Hide whole column if it only contains this card outside edit mode.
-    cch_card.parentNode.style.cssText = alone ? 'display:none' : '';
+  // Find the card element.
+  recursive_walk(app_layout, function(node) {
+    card = node.nodeName == 'COMPACT-CUSTOM-HEADER' ? node : null;
+  });
+  // Hide whole column if this card is the only one it contains.
+  if (card != null) {
+    if (card.parentNode.children.length == 1) {
+      card.parentNode.style.cssText = 'display:none';
+    } else {
+      card.parentNode.style.cssText = '';
+    }
+    // Create and display card in edit mode.
+    if (edit_mode) {
+      card.style.cssText = '';
+      card.innerHTML = `
+        <svg style="float:left;height:30px;padding:15px;padding-right:5px;"
+        viewBox="0 0 24 24"><path fill="var(--primary-text-color)"
+        d="M12,7L17,12H14V16H10V12H7L12,7M19,21H5A2,2 0 0,1 3,19V5A2,2 0 0,1 5,
+        3H19A2,2 0 0,1 21,5V19A2,2 0 0,1 19,21M19,19V5H5V19H19Z"></path></svg>
+        <h2 style="margin:auto;padding:20px;">Compact Custom Header</h2>
+      `;
+      card.parentNode.style.cssText = 'background-color:var(--primary-color)';
+    } else {
+      // Hide card outside of edit mode.
+      card.style.cssText = 'display:none';
+      card.innerHTML = '';
+    }
   }
+
+  window.dispatchEvent(new Event('resize'));
 
   // Style header and icons if "disable: false" in config, which is default.
   if (!window.cch_disable) {
     // Hide scroll arrows on tab bar to save space.
-    tab_chevron[0].style.cssText = 'display:none;';
-    tab_chevron[1].style.cssText = 'display:none;';
+    chevron[0].style.cssText = 'display:none;';
+    chevron[1].style.cssText = 'display:none;';
     // Pad bottom for image backgrounds as we're shifted -64px.
     if (window.cch_background_image) {
       div_view.style.paddingBottom = '64px';
@@ -159,7 +103,7 @@ if (proceed) {
 
     // Shift the header up to hide unused portion, but only with multiple tabs.
     // When there is only one tab the header is already collapsed.
-    if (tab_count.length > 1) {
+    if (tabs_count.length > 1) {
       hui_root.querySelector('app-toolbar').style.cssText = 'margin-top:-64px;';
     }
 
@@ -181,30 +125,26 @@ if (proceed) {
       tabs_container.style.cssText = `margin-left:${clock_width + 15}px;`;
     }
 
+    // Get elements to style for clock choice.
     if (window.cch_clock) {
-      // Get elements to style for clock choice.
       if (window.cch_clock == 'notification') {
         icon = notify_icon;
-        shadow_root = notify_icon_sr;
         iron_icon = notify_iron_icon;
         notify_indicator.style.cssText = 'top:14.5px;left:-7px';
       } else if (window.cch_clock == 'voice') {
         icon = voice_icon;
-        shadow_root = voice_icon_sr;
         iron_icon = voice_iron_icon;
       } else if (window.cch_clock == 'options') {
         icon = options_icon;
-        shadow_root = options_icon_sr;
         iron_icon = options_iron_icon;
       } else if (window.cch_clock == 'menu') {
         icon = menu_icon;
-        shadow_root = menu_icon_sr;
         iron_icon = menu_iron_icon;
       }
 
       // Find inserted clock element.
       try {
-        clock = shadow_root.getElementById('cch_clock');
+        clock = icon.shadowRoot.getElementById('cch_clock');
       } catch (e) {
         console.log(e);
       }
@@ -239,8 +179,8 @@ if (proceed) {
     }
   } else if (window.cch_disable) {
     // Clear any styles that were modified if disabled.
-    tab_chevron[0].style.cssText = '';
-    tab_chevron[1].style.cssText = '';
+    chevron[0].style.cssText = '';
+    chevron[1].style.cssText = '';
     hui_root.querySelector('app-header').style.cssText = '';
     hui_root.querySelector('app-toolbar').style.cssText = '';
     tabs.style.cssText = '';
@@ -253,25 +193,37 @@ if (proceed) {
   }
 
   // Hide or show buttons and tab container
-  element_style(window.cch_menu, menu_button, true);
-  element_style(window.cch_notify, notify_button, true);
-  element_style(window.cch_voice, voice_button, true);
+  element_style(window.cch_menu, menu_btn, true);
+  element_style(window.cch_notify, notify_btn, true);
+  element_style(window.cch_voice, voice_btn, true);
   element_style(window.cch_options, options_button, true);
-  for (let i = 0; i < tab_count.length; i++) {
-    element_style(window.cch_tabs, tab_count[i], false);
+  for (let i = 0; i < tabs_count.length; i++) {
+    element_style(window.cch_tabs, tabs_count[i], false);
   }
   // Resize to update header.
   window.dispatchEvent(new Event('resize'));
 }
 
+// Walk the DOM to find card element.
+function recursive_walk(node, func) {
+    var done = func(node) || node.nodeName == 'COMPACT-CUSTOM-HEADER';
+    if (done) return true;
+    if ('shadowRoot' in node && node.shadowRoot) {
+        done = recursive_walk(node.shadowRoot, func);
+        if (done) return true;
+    }
+    node = node.firstChild;
+    while (node) {
+        done = recursive_walk(node, func);
+        if (done) return true;
+        node = node.nextSibling;
+    }
+}
+
 // Style or hide buttons and tabs.
 function element_style(config, element, shift) {
-  if (edit_mode) {
-    shift_amount = 240;
-  } else {
-    shift_amount = 111;
-  }
-  if (tab_count.length > 1 && shift && !window.cch_disable) {
+  shift_amount = edit_mode ? 240 : 111;
+  if (tabs_count.length > 1 && shift && !window.cch_disable) {
     element.style.cssText = config ?
       `z-index:1; margin-top:${shift_amount}px;` :
       'display:none' ;
