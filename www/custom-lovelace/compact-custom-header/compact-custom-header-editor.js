3ef3bf4f9508e412bf4c650102091d4db1804b78
DavidFW1960 <36390762+DavidFW1960@users.noreply.github.com>
2019-02-19 15:29:09 +1100

    cards & lovelace move cards

--- www/custom-lovelace/compact-custom-header/compact-custom-header-editor.js
+++ www/custom-lovelace/compact-custom-header/compact-custom-header-editor.js
@@ -27,7 +27,8 @@ const defaultConfig = {
   disable: false,
   background_image: false,
   main_config: false,
-  hide_tabs: []
+  hide_tabs: [],
+  show_tabs: []
 };
 
 export class CompactCustomHeaderEditor extends LitElement {
@@ -200,6 +201,10 @@ export class CchConfigEditor extends LitElement {
     return this.config.hide_tabs || this.defaultConfig.hide_tabs || "";
   }
 
+  get _show_tabs() {
+    return this.config.show_tabs || this.defaultConfig.show_tabs || "";
+  }
+
   get _clock() {
     return (
       this._menu == "clock" ||
@@ -330,7 +335,7 @@ export class CchConfigEditor extends LitElement {
           : ""}
       </div>
 
-      <h4>Button Visability:</h4>
+      <h4>Button Visibility:</h4>
       <div class="buttons side-by-side">
         <div
           class="${this.exception && this.config.menu === undefined
@@ -459,18 +464,56 @@ export class CchConfigEditor extends LitElement {
             </div>
           `
         : ""}
-      <h4>Hide Tabs:</h4>
-      <paper-input
-        class="${this.exception && this.config.hide_tabs === undefined
-          ? "inherited"
-          : ""}"
-        label="Comma-separated list of tab numbers to hide:"
-        .value="${this._hide_tabs}"
-        .configValue="${"hide_tabs"}"
-        @value-changed="${this._valueChanged}"
+      <h4>Tab Visibility:</h4>
+        <paper-dropdown-menu id="tabs" @value-changed="${this._tabVisibility}">
+        <paper-listbox slot="dropdown-content"
+          .selected="${this._show_tabs.length > 0 ? "1" : "0"}"
+        >
+          <paper-item>Hide Tabs</paper-item>
+          <paper-item>Show Tabs</paper-item>
+        </paper-listbox>
+      </paper-dropdown-menu>
+      <div id="show"
+        style="display:${this._show_tabs.length > 0 ? "initial" : "none"}"
       >
-      </paper-input>
+        <paper-input
+          class="${this.exception && this.config.show_tabs === undefined
+            ? "inherited"
+            : ""}"
+          label="Comma-separated list of tab numbers to show:"
+          .value="${this._show_tabs}"
+          .configValue="${"show_tabs"}"
+          @value-changed="${this._valueChanged}"
+        >
+        </paper-input>
+      </div>
+      <div id="hide"
+        style="display:${this._show_tabs.length > 0 ? "none" : "initial"}"
+      >
+        <paper-input
+          class="${this.exception && this.config.hide_tabs === undefined
+            ? "inherited"
+            : ""}"
+          label="Comma-separated list of tab numbers to hide:"
+          .value="${this._hide_tabs}"
+          .configValue="${"hide_tabs"}"
+          @value-changed="${this._valueChanged}"
+        >
+        </paper-input>
+      </div>
     `;
+  }
+
+  _tabVisibility() {
+    let show = this.shadowRoot.querySelector('[id="show"]');
+    let hide = this.shadowRoot.querySelector('[id="hide"]');
+    if (this.shadowRoot.querySelector('[id="tabs"]').value == "Hide Tabs") {
+      show.style.display = "none";
+      hide.style.display = "initial";
+    } else {
+      hide.style.display = "none";
+      show.style.display = "initial";
+    }
   }
 
   _valueChanged(ev) {
