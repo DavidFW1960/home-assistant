6d375deeea9cd846a45876cca3fe54f50e188834
DavidFW1960 <36390762+DavidFW1960@users.noreply.github.com>
2019-04-09 15:35:39 +1000

    abb scripts, new iOS mobile_app

--- abbusage/abbcreds.sh
+++ abbusage/abbcreds.sh
@@ -2,6 +2,7 @@
 
 cd "$(dirname "$0")"
 
+clear
 echo "This script will prompt you for your Aussie Broadband (ABB) and Home Assistant Login Credentials"
 
 if [[ -e "abbcreds.json" ]]
@@ -14,16 +15,23 @@ abblogin=$(echo "$abbcreds" | jq -r '.abblogin')
 read -e -i "$abblogin" -p "ABB Username: " input
 abblogin="${input:-$abblogin}"
 
+echo ""
 echo "Enter your ABB Password, followed by [ENTER]:"
 abbpassword=$(echo "$abbcreds" | jq -r '.abbpassword')
 read -e -i "$abbpassword" -p "ABB Password: " input
 abbpassword="${input:-$abbpassword}"
 
+cookie=abbcookie.txt
+curl --silent -c $cookie -d "username=$abblogin" -d "password=$abbpassword" --url 'https://myaussie-auth.aussiebroadband.com.au/login' > abbtoken.json
+usageid=$(curl --silent -b $cookie --url "https://myaussie-api.aussiebroadband.com.au/customer" |jq -r '.services.NBN[0].service_id')
+echo ""
+echo "The Usage ID we detect is shown."
+echo "Only change the Usage ID if it is detected incorrectly otherwise press [Enter]"
 echo "Enter your ABB Service ID, followed by [ENTER]:"
-usageid=$(echo "$abbcreds" | jq -r '.usageid')
-read -e -i "$usageid" -p "ABB Service ID: " input
+read -e -i "$usageid" -p "Detected ABB Service ID: " input
 usageid="${input:-$usageid}"
 
+echo ""
 echo "Enter your Home Assistant Server IP/URL and port eg http://10.90.11.100:8123, followed by [ENTER]:"
 server=$(echo "$abbcreds" | jq -r '.server')
 if [ -z "$server" ] 
@@ -35,11 +43,13 @@ fi
 read -e -i "$server" -p "Home Assistant Server: " input
 server="${input:-$server}"
 
+echo ""
 echo "Enter your Home Assistant Long Access Token (184 characters), followed by [ENTER]:"
 token=$(echo "$abbcreds" | jq -r '.token')
 read -e -i "$token" -p "HA Long Lived Token: " input
 token="${input:-$token}"
 
+echo ""
 echo "Enter your Home Assistant Server Entity Picture eg /local/icons/abb/abb.png (default) followed by [ENTER]:"
 entitypicture=$(echo "$abbcreds" | jq -r '.entitypicture')
 if [ -z "$entitypicture" ]
@@ -53,6 +63,5 @@ entitypicture="${input:-$entitypicture}"
 
 abbcreds='{"abblogin":"'"$abblogin"'","abbpassword":"'"$abbpassword"'","usageid":"'"$usageid"'","server":"'"$server"'","token":"'"$token"'","entitypicture":"'"$entitypicture"'"}'
 echo $abbcreds  > abbcreds.json
-
-cookie=abbcookie.txt
-curl -c $cookie -d "username=$abblogin" -d "password=$abbpassword" --url 'https://myaussie-auth.aussiebroadband.com.au/login' > abbtoken.json
+echo ""
+echo "Your credentials have been saved"
