e73050048c2298887743eac9a6eab6cfe03cef39
DavidFW1960 <36390762+DavidFW1960@users.noreply.github.com>
2019-08-14 15:12:09 +1000

    card updates

--- www/community/text-divider-row/text-divider-row.js
+++ www/community/text-divider-row/text-divider-row.js
@@ -1,60 +1,62 @@
-var LitElement =
-  LitElement ||
-  Object.getPrototypeOf(customElements.get("home-assistant-main"));
-var html = LitElement.prototype.html;
+const LitElement = Object.getPrototypeOf(customElements.get("hui-view"));
+const html = LitElement.prototype.html;
 
 class TextDividerRow extends LitElement {
-  static get properties() {
-    return {
-      _config: {}
-    };
-  }
-
-  setConfig(config) {
-    if (!config || !config.text) {
-      throw new Error("Error in card configuration.");
+    static get properties() {
+        return {
+            _config: {}
+        };
     }
 
-    this._config = config;
-  }
-
-  render() {
-    if (!this._config) {
-      return html``;
+    getCardSize() {
+        return 1;
     }
 
-    return html`
-      ${this.renderStyle()}
+    setConfig(config) {
+        if (!config || !config.text) {
+            throw new Error("Error in card configuration.");
+        }
+
+        this._config = config;
+    }
+
+    render() {
+        if (!this._config) {
+            return html``;
+        }
+
+        return html`
+    ${this.renderStyle()}
       <h2 class="text-divider"><span>${this._config.text}</span></h2>
     `;
-  }
+    }
 
-  renderStyle() {
-    return html`
+    renderStyle() {
+        return html`
       <style>
-        .text-divider {
-          margin: 1em 0;
-          line-height: 0;
-          text-align: center;
-          white-space: nowrap;
-          display: flex;
-          align-items: center;
-        }
-        .text-divider span {
-          padding: 1em;
-          color: var(--secondary-text-color);
-        }
-        .text-divider:before,
-        .text-divider:after  {
-          content: '';
-          background: var(--secondary-text-color);
-          display: block;
-          height: 1px;
-          width: 100%;
-        }
+      .text-divider {
+        margin: 1em 0;
+        line-height: 0;
+        text-align: center;
+        white-space: nowrap;
+        display: flex;
+        align-items: center;
+      }
+      .text-divider span {
+        padding: 1em;
+        color: var(--secondary-text-color);
+      }
+      .text-divider:before,
+      .text-divider:after {
+        content: "";
+        background: var(--secondary-text-color);
+        display: block;
+        height: 1px;
+        width: 100%;
+      }
       </style>
     `;
-  }
+    }
 }
 
 customElements.define("text-divider-row", TextDividerRow);
