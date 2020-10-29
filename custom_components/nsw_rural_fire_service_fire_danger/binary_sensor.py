e76f578a999086b64cb836ab3b1b1addce860f73
DavidFW1960 <36390762+DavidFW1960@users.noreply.github.com>
2020-10-29 13:36:06 +1100

    update nsw fire incidents sensor

--- custom_components/nsw_rural_fire_service_fire_danger/binary_sensor.py
+++ custom_components/nsw_rural_fire_service_fire_danger/binary_sensor.py
@@ -1,12 +1,16 @@
-"""NSW Rural Fire Service - Fire Danger - Sensor."""
+"""NSW Rural Fire Service - Fire Danger - Binary Sensor."""
 import logging
-from typing import Optional, Callable, List
+from typing import Callable, List
 
+from homeassistant.components.binary_sensor import (
+    BinarySensorEntity,
+    DEVICE_CLASS_SAFETY,
+)
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity import Entity
 
-from .const import DOMAIN, SENSOR_TYPES
+from .const import DOMAIN, BINARY_SENSOR_TYPES
 from .entity import NswFireServiceFireDangerEntity
 
 _LOGGER = logging.getLogger(__name__)
@@ -26,25 +30,27 @@ async def async_setup_entry(
 
     async_add_entities(
         [
-            NswFireServiceFireDangerSensor(
+            NswFireServiceFireDangerBinarySensor(
                 hass, manager, sensor_type, config_entry_unique_id
             )
-            for sensor_type in SENSOR_TYPES
+            for sensor_type in BINARY_SENSOR_TYPES
         ],
         True,
     )
-    _LOGGER.debug("Sensor setup done")
+    _LOGGER.debug("Binary sensor setup done")
 
 
-class NswFireServiceFireDangerSensor(NswFireServiceFireDangerEntity):
-    """Implementation of the sensor."""
-
-    @property
-    def state(self):
-        """Return the state of the device."""
-        return self._state
+class NswFireServiceFireDangerBinarySensor(
+    NswFireServiceFireDangerEntity, BinarySensorEntity
+):
+    """Implementation of the binary sensor."""
 
     @property
-    def icon(self) -> Optional[str]:
-        """Return the icon to use in the frontend, if any."""
-        return "mdi:speedometer-medium"
+    def is_on(self):
+        """Return true if the binary sensor is on."""
+        return bool(self._state)
+
+    @property
+    def device_class(self):
+        """Return the class of this device."""
+        return DEVICE_CLASS_SAFETY
