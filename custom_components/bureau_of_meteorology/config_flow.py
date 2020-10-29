de6494e16c22a8dadfc6481ac6d1900a49c28b55
DavidFW1960 <36390762+DavidFW1960@users.noreply.github.com>
2020-10-29 13:33:14 +1100

    update BOM component

--- custom_components/bureau_of_meteorology/config_flow.py
+++ custom_components/bureau_of_meteorology/config_flow.py
@@ -29,15 +29,17 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         if user_input is not None:
             try:
                 collector = Collector(
-                    self.hass.config.latitude,
-                    self.hass.config.longitude
+                    user_input[CONF_LATITUDE],
+                    user_input[CONF_LONGITUDE],
                 )
+                await collector.get_location_name()
                 result = await collector.get_observations_data()
                 if result is None:
                     raise CannotConnect
 
-                name = collector.observations_data["data"]["station"]["name"]
-                return self.async_create_entry(title=name, data=user_input)
+                #name = collector.observations_data["data"]["station"]["name"]
+                #name = collector.locations_data
+                return self.async_create_entry(title=collector.location_name, data=user_input)
             except CannotConnect:
                 errors["base"] = "cannot_connect"
             except Exception:
